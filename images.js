41a0b0171a9c7a0ea13053a4d92347f8ecc18126
Mateusz Hallala <matt@huncwot.com>
2022-07-06 16:30:50 +0200

    seems to work

--- images.js
+++ images.js
@@ -13,7 +13,7 @@ async function optimizePNG(filePath) {
   const stats = await Image( filePath, {
         widths: [null],
         formats: ['webp', 'png'],
-        outputDir: outputDir,
+        outputDir: currentOutputPath,
         filenameFormat: (id, src, width, format) => {
             return `${parse(filePath).name}.${format}`
         },
@@ -21,6 +21,7 @@ async function optimizePNG(filePath) {
 }
 
 let currentPath = inputDir;
+let currentOutputPath = outputDir
 // filePath
 async function iterateRecursively(dir) {
     let entries = await readdir(dir, { withFileTypes: true });
@@ -34,7 +35,11 @@ async function iterateRecursively(dir) {
         } else {
             if (!fs.existsSync(`${outputDir}/${entry.name}`)) fs.mkdirSync(`${outputDir}/${entry.name}`);
             console.log('i am a folder:', entry);
+            currentPath += `${entry.name}/`;
+            currentOutputPath += `${entry.name}/`;
+            console.log('the new current path:', currentPath);
             // iterateRecursively(`${inputDir}/${entry.name}`)
+            iterateRecursively(currentPath)
         }
     })
 }
