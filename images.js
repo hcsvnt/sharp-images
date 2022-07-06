a336a8ea56056d214af01224b718eeb7666afeba
Mateusz Hallala <matt@huncwot.com>
2022-07-06 16:25:44 +0200

    cleanup

--- images.js
+++ images.js
@@ -4,101 +4,39 @@ const { readdir } = require('fs/promises')
 const { parse } = require('path')
 
 
-// BE AWARE: 16x16px IS THE MINIMAL SIZE FOR AVIF PROCESSING!!!
-
-
 const inputDir = './input/';
-const outputDir = './output';
+const outputDir = './output/';
 
 
-async function optimizePNG(file, subDir) {
-  const stats = await Image(inputDir + file, {
+async function optimizePNG(filePath) {
+    console.log('file to optimize in 11-ty:', filePath)
+  const stats = await Image( filePath, {
         widths: [null],
         formats: ['webp', 'png'],
-        sharpJpegOptions: {
-            quality: 60
-        },
-        sharpWebpOptions: {
-            quality: 60
-        },
-        sharpPngOptions: {
-            quality: 50,
-            compressionLevel: 9,
-            effort: 10 // default: 7
-        },
-        sharpAvifOptions: {
-            quality: 70,
-            chromaSubsampling: '4:2:0',
-            effort: 6 // default: 4
-        },
-        outputDir: subDir ? outputDir + '/' + subDir : outputDir,
+        outputDir: outputDir,
         filenameFormat: (id, src, width, format) => {
-            if (format.toLocaleLowerCase() === 'jpeg') return `${parse(file).name}.jpg`
-            // override the default '.jpeg' file extension
-            return `${parse(file).name}.${format}`
+            return `${parse(filePath).name}.${format}`
         },
     })
-    // console.log(stats)
 }
 
-async function optimizeJPG(file, subDir) {
-    const stats = await Image(inputDir + file, {
-        widths: [null],
-        formats: ['jpg', 'webp'],
-        sharpJpegOptions: {
-            quality: 60
-        },
-        sharpWebpOptions: {
-            quality: 60
-        },
-        sharpAvifOptions: {
-            quality: 70,
-            chromaSubsampling: '4:2:0',
-            effort: 6 // default: 4
-        },
-        outputDir: subDir ? outputDir + '/' + subDir : outputDir,
-        filenameFormat: (id, src, width, format) => {
-            if (format.toLocaleLowerCase() === 'jpeg') return `${parse(file).name}.jpg`
-            // override the default '.jpeg' file extension
-            return `${parse(file).name}.${format}`
-        }
-    })
-    // console.log(stats)
-}
-
-
-async function iterateRecursively(dir, subDir) {
+let currentPath = inputDir;
+// filePath
+async function iterateRecursively(dir) {
     let entries = await readdir(dir, { withFileTypes: true });
+    console.log('entries:', entries)
     entries.forEach(entry => {
         if (!entry.isDirectory()) {
-            console.log('this', entry, parse(entry.name))
             const fileExtension = parse(entry.name).ext.toLowerCase();
+            console.log('file:', entry.name, 'ext:', fileExtension)
 
-            console.log(entry.name, subDir);
-            if (fileExtension === '.png') optimizePNG(entry.name, subDir);
-            if (fileExtension === '.jpg' || fileExtension === '.jpeg') optimizeJPG(entry.name, subDir);
-
+            if (fileExtension === '.png') optimizePNG(currentPath + entry.name);
         } else {
-            if (!fs.existsSync(`${outputDir}/${entry.name}`)) {
-                fs.mkdirSync(`${outputDir}/${entry.name}`);
-            }
-            console.log(`${inputDir}${entry.name}`, entry.name);
-            // jak to odkomentujesz, to się spierdoli, ale właśnie próbuję dojsc dlaczego
-            // iterateRecursively(`${inputDir}${entry.name}`, entry.name);
+            if (!fs.existsSync(`${outputDir}/${entry.name}`)) fs.mkdirSync(`${outputDir}/${entry.name}`);
+            console.log('i am a folder:', entry);
+            // iterateRecursively(`${inputDir}/${entry.name}`)
         }
     })
 }
 
-iterateRecursively(inputDir, null);
-
-// to działało bez subfolderów
-// (async () => {
-//     const files = await readdir(inputDir, {withFileTypes: true})
-//     for (const file of files) {
-
-//         console.log(file.isDirectory(), file.name);
-//         // const fileExtension = parse(file).ext.toLowerCase();
-//         // if (fileExtension === '.png') await optimizePNG(file);
-//         // if (fileExtension === '.jpg' || fileExtension === '.jpeg') await optimizeJPG(file);
-//     }
-// })()
+iterateRecursively(inputDir);
