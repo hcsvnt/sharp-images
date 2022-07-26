6760a2eb835aa2ed5b1c7eb8c1e096a9f016644f
Piotr Bekas <piotr.bekas@gmail.com>
2022-07-26 16:02:35 +0200

    Save dir structure

--- sharp_images.js
+++ sharp_images.js
@@ -1,12 +1,18 @@
 const sharp = require('sharp');
-const { readdirSync, statSync } = require('fs');
-const { join, basename, extname } = require('path')
-const inputDir = './input';
-const outputDir = '/output';
-const possibleFormats = ['.jpg', '.jpeg', '.png', '.webp', '.avif']
+const { readdirSync, statSync, mkdirSync, existsSync } = require('fs');
+const { join, basename, extname, parse, relative, dirname, sep } = require('path')
+const inputDir = 'input';
+const outputDir = 'output';
+const possibleFormats = ['.jpg', '.jpeg', '.png']
+const outputs = {
+  jpeg: ['jpg', 'webp', 'avif'],
+  png: ['png', 'webp', 'avif']
+}
 
-function isImage(file) {
-  return possibleFormats.includes(extname(file))
+function inspectImage(file) {
+  const extName = extname(file);
+  const isImage = possibleFormats.includes(extName);
+  return { isImage, extName }
 }
 
 function getAllImageFiles(dirPath, arrayOfFiles) {
@@ -17,7 +23,7 @@ function getAllImageFiles(dirPath, arrayOfFiles) {
     if (statSync(dirPath + "/" + file).isDirectory()) {
       arrayOfFiles = getAllImageFiles(dirPath + "/" + file, arrayOfFiles);
     } else {
-      if (!isImage(file)) {
+      if (!inspectImage(file).isImage) {
         return
       }
       arrayOfFiles.push(join(__dirname, dirPath, "/", file));
@@ -28,38 +34,68 @@ function getAllImageFiles(dirPath, arrayOfFiles) {
 }
 
 async function handleImage(inputPath) {
+  // console.log({inputPath: parse(inputPath)})
+  
+  const relativeOutputPath = relative(__dirname, inputPath);
+  const parsedPath = relativeOutputPath.split(sep)
+  parsedPath[0] = outputDir;
+  const finalOutputPath = parsedPath.join(sep);
+  const finalDirName = dirname(finalOutputPath);
+  const fileDetails = inspectImage(inputPath);
+  const asdf = __dirname + '/' + finalDirName;
 
-  if (!isImage(inputPath)) {
+
+  if (!fileDetails.isImage) {
     return
   }
 
-  const imageBaseName = basename(inputPath);
-  const imageOutputPath = __dirname + outputDir + '/' + imageBaseName;
+  if (!existsSync(asdf)){
+    mkdirSync(asdf);
+  }
 
-  await sharp(inputPath)
-    .jpeg({ 
-        mozjpeg: true,
+
+  const imageBaseName = basename(inputPath, fileDetails.extName);
+  const outputType = fileDetails.extName === '.jpg' || fileDetails.extName === '.jpeg' ? 'jpeg' :  fileDetails.extName === '.png' ? 'png' : null;
+  
+  if (!outputType) {
+    return 
+  }
+
+  const outputsByExtType = outputs[outputType];
+
+  outputsByExtType.forEach(async (ext) => {
+  // const imageOutputPath = __dirname + '/' + outputDir + '/' + imageBaseName + '.' + ext;
+  const imageOutputPath = __dirname + '/' + finalDirName + '/' + imageBaseName + '.' + ext;
+  
+    await sharp(inputPath)
+      .toFormat(outputType)
+      .jpeg({ 
+          mozjpeg: true,
+          quality: 60,
+      })
+      .png({
         quality: 60,
-    })
-    .png({
-      quality: 60,
-      compressionLevel: 9,
-      effort: 10,
-    })
-    .webp({
-      quality: 70,
-    })
-    .avif({
-      quality: 60,
-      chromaSubsampling: '4:2:0',
-      effort: 6
-    })
-    .toFile(imageOutputPath, (err, info) => {
-      if (err) {
-        console.error({err})
-      }
-    });
+        compressionLevel: 9,
+        effort: 10,
+      })
+      .webp({
+        quality: 70,
+      })
+      .avif({
+        quality: 60,
+        chromaSubsampling: '4:2:0',
+        effort: 6
+      })
+      .toFile(imageOutputPath, (err, info) => {
+        if (err) {
+          console.error({err})
+        }
+      });
+
+  })
+
 }
 
-const allFiles = getAllImageFiles(inputDir);
+const allFiles = getAllImageFiles('./' + inputDir);
+// console.log({allFiles, dir: __dirname})
 allFiles.forEach(image => handleImage(image));
