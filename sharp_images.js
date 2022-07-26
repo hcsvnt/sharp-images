e4710da835ce4b884369a0a763aa0f690139c9ea
Piotr Bekas <piotr.bekas@gmail.com>
2022-07-26 12:26:39 +0200

    Cleaning

--- sharp_images.js
+++ sharp_images.js
@@ -1,19 +1,18 @@
 const sharp = require('sharp');
-const fs = require('fs');
-const path = require('path')
+const { readdirSync, statSync } = require('fs');
+const { join, basename } = require('path')
 const inputDir = './input';
 const outputDir = '/output';
 
-const getAllFiles = function(dirPath, arrayOfFiles) {
-  files = fs.readdirSync(dirPath)
-
-  arrayOfFiles = arrayOfFiles || []
+function getAllFiles(dirPath, arrayOfFiles) {
+  files = readdirSync(dirPath)
+  arrayOfFiles = arrayOfFiles || [];
 
   files.forEach(function(file) {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+    if (statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
     } else {
-      arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+      arrayOfFiles.push(join(__dirname, dirPath, "/", file));
     }
   })
 
@@ -21,7 +20,7 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
 }
 
 async function JPG(inputPath) {
-  const imageFileName = path.basename(inputPath);
+  const imageFileName = basename(inputPath);
 
   await sharp(inputPath)
     .jpeg({ mozjpeg: true })
@@ -33,5 +32,5 @@ async function JPG(inputPath) {
     });
 }
 
-const allFiles = getAllFiles(inputDir)
-allFiles.forEach(image => JPG(image))
+const allFiles = getAllFiles(inputDir);
+allFiles.forEach(image => JPG(image));
